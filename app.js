c9ab1bc38d3641d741dc0724bfe201d51d86963f
hariharan <hariharanjayachandran04@gmail.com>
2025-01-22 20:53:04 +0530

    test deploy

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("hello new from updated api");
 })
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/customer', customerRoutes);
