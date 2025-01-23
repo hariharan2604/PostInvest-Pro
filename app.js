0a7ca8dbf1fff5fe7a7b30e3793afd4ccd80dc97
Hariharan Jayachandran <92015708+hariharan2604@users.noreply.github.com>
2025-01-23 15:23:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("hello new 23-01");
+  res.send("hello new 23-01-2025");
 })
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/customer', customerRoutes);
